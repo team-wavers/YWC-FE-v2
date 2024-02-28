5b26db795a6282b722bafd5a6f80328fdb77ef16
dyslo <auau3@kakao.com>
2024-02-28 15:45:35 +0900

    feature: meta data, opengraph data 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,11 +36,26 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <title>전남청년 문화복지카드 가맹점 검색</title>
+                    <link rel="icon" href="/favicon.ico" />
                     <meta charSet="utf-8" />
                     <meta
                         name="viewport"
                         content="width=device-width, initial-scale=1.0, user-scalable=0"
                     />
+                    <meta
+                        name="description"
+                        content="전남청년 문화복지카드 가맹점 검색 서비스"
+                    />
+                    <meta content="#f1f2f4" name="theme-color" />
+                    <meta content="https://youthwelfare.kr" property="og:url" />
+                    <meta content="website" property="og:type" />
+                    <meta
+                        content="문화복지카드 가맹점을 쉽게 검색해보세요!"
+                        property="og:description"
+                    />
+                    <meta content="./thumbnail.png" property="og:image" />
+                    <meta content="1200" property="og:image:width" />
+                    <meta content="630" property="og:image:height" />
                 </Head>
                 <body>
                     <Main />
