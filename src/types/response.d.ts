1aa67d26ff41afb3c4beee00dd958bd15c0a5bb3
용원 <auau3@kakao.com>
2024-03-19 19:35:15 +0900

    feature: 가맹점별 결제 가능 은행 추가

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -1,11 +1,15 @@
 interface GeneralResponse<T> {
     code: number;
-    message: string;
+    message: ResponseMessage;
     result: { count: number; rows: T };
 }
 
 interface ResponseMessage {
-    message: {
-        OK: "api.common.ok";
-    };
+    OK: "api.common.ok";
+    BAD_REQUEST: "api.common.bad_request";
+    UNAUTHORIZED: "api.common.unauthorized";
+    FORBIDDEN: "api.common.forbidden";
+    NOT_FOUND: "api.common.not_found";
+    CONFLICT: "api.common.conflict";
+    INTERNAL_SERVER_ERROR: "api.common.error";
 }
